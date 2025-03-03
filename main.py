f9cccea55787d42962664d2dd9ee2dfe6eb1e826
0xquick <0xquick.eth@gmail.com>
2025-03-03 18:49:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -100,4 +100,5 @@ def serve_game():
     return open("arcade_snake.html").read()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
